c19b871ee6612c2ba34b50075c24c92c0a8f930b
dD7&ee8(112rr <francesco.re.nttdata@skytv.it>
2022-02-22 15:48:29 +0100

    COMMIT TEST VARIE

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -4,6 +4,6 @@ import { error } from 'console';
 export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
-    body: `Hello, CDK NEWWW ! You have hit ${event.path}\n`
+    body: `Hello, CDK NEWWW, CIAO CIAO ! You have hit ${event.path}\n`
   };
 };
